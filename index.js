f34a3b5ff05ace96972d825be5a972913297126d
james sewell <jamesjosephsewell@gmail.com>
2018-10-19 11:17:09 -0500

    pead pudding

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 console.log('sup')
 let list = document.getElementById('list')
 
-
-let nothinBtn = document.getElementById("nothing")
-
+let nothinBtn = document.getElementById('nothing')
 
 nothinBtn.addEventListener('click', () => {
-    document.write("AHHHHHHHHH I LIEDDDDDDD")
+  document.write('AHHHHHHHHH I LIEDDDDDDD')
+  document.write('peas pudding hot')
+  document.write('peas pudding cold')
+  document.write('peas pudding in the pot nine days old')
 })
 console.log('foo')
 console.log('bar')
