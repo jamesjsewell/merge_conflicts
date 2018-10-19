e291e2e2fa450a5ec7bd654f1e6e78636c69c4a9
Stephen Roberts <Sroberts12321@gmail.com>
2018-10-19 11:12:50 -0500

    break damn you

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log('sup')
+let list = document.getElementById('list')
+
+
+let nothinBtn = document.getElementById("nothing")
+
+
+nothinBtn.addEventListener('click', () => {
+    document.write("AHHHHHHHHH I LIEDDDDDDD")
+})
