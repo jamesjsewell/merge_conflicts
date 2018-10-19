632d54a8eb063b2475f3748bfd741c4f0b4030f0
Stephen Roberts <Sroberts12321@gmail.com>
2018-10-19 11:20:18 -0500

    data

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 console.log('sup')
 let list = document.getElementById('list')
-
+let last = document.getElementById('last')
 let nothinBtn = document.getElementById('nothing')
 
 nothinBtn.addEventListener('click', () => {
-  document.write('AHHHHHHHHH I LIEDDDDDDD')
-  document.write('peas pudding hot')
-  document.write('peas pudding cold')
-  document.write('peas pudding in the pot nine days old')
+    last.innerHTML += `AHHHHHHHHH I LIEDDDDDDD`
 })
-console.log('foo')
-console.log('bar')
-console.log('baz')
-console.log('bar')
-console.log('foo')
 console.log(1 + 2)
